7ccec399de72163356ba6b0200e27acdfc516b87
Viacheslav Lamash <slava91billion@gmail.com>
2018-03-29 15:47:53 +0300

    Create class mystring

--- index.js
+++ index.js
@@ -1,16 +1,79 @@
-class Worker{
-    constructor(name, surname, rate, days){
-        this.name = name;
-        this.surname = surname;
-        this.rate = rate;
-        this.days = days;
+//class Worker{
+//    constructor(name, surname, rate, days){
+//        this._name = name;
+//        this._surname = surname;
+//        this._rate = rate;
+//        this._days = days;
+//    }
+//    
+//    getSalery(){
+//        return this._rate*this._days;
+//        
+//    }
+//    
+//    getName(){
+//        return this._name;
+//        
+//    }
+//    
+//    getSurname(){
+//        return this._surname;
+//        
+//    }
+//    
+//    getRate(){
+//        return this._rate;
+//        
+//    }
+//    
+//    getDays(){
+//        return this._days;
+//        
+//    }
+//    
+//    setRate(newRate){
+//        return this._rate=newRate;
+//        
+//    }
+//    
+//    setDays(newDays){
+//        return this._days=newDays;
+//        
+//    }
+//}
+//
+//let worker = new Worker('Ivan', 'Pitul', 10, 60)
+//worker.setDays(50)
+//console.log(worker.getSalery())
+
+
+class MyString{
+    reverse(str){
+//        return str.split(' ').reverse().forEach(x=>{
+//            return x.split('').reverse().join()
+//        }).join(' ')
+        return str.split(' ').reverse().map(x=>x.split('').reverse().join('')).join(' ')
     }
     
-    getSalery(){
-        return this.rate*this.days;
-        
+    ucFirst(str){
+        return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+    
+    ucWord(str){
+        return str.split(' ')
+            .map(x=> x.charAt(0).toUpperCase()+ x.slice(1))
+            .join(' ')
     }
 }
 
-let worker = new Worker('Ivan', 'Pitul', 10, 60)
-console.log(worker.getSalery())
+let string = new MyString()
+
+console.log(string.ucWord('de gh ki'))
+
+
+
+
+
+
+
+
